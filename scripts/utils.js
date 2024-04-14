cfb16eac01f86986ac123d046b123306c774fc17
Clemens Verhoosel <c.v.verhoosel@tue.nl>
2024-04-14 08:39:40 +0200

    Added Track 3 description

--- scripts/utils.js
+++ scripts/utils.js
@@ -4,6 +4,11 @@ function setLinks() {
     setLink('presentation', "https://canvas.tue.nl/courses/24022/assignments/118572");
     setLink('competition', "https://canvas.tue.nl/courses/24022/assignments/118569");
     setLink('gonogo', "https://canvas.tue.nl/courses/24022/assignments/118568");
+    setLink('practical', "https://canvas.tue.nl/courses/24022/assignments/118570");
+    setLink('measurementtheory', "https://canvas.tue.nl/courses/24022/quizzes/24284");
+    setLink('sopv1', "https://canvas.tue.nl/courses/24022/assignments/118615");
+    setLink('sopv2', "https://canvas.tue.nl/courses/24022/assignments/118685");
+    setLink('sopv3', "https://canvas.tue.nl/courses/24022/assignments/118686");
 }
 
 function setLink(classname, url) {
