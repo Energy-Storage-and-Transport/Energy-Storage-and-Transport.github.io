6b6987b275da0a3c86ec7aded2a06d44c9ac68dc
Clemens Verhoosel <c.v.verhoosel@tue.nl>
2025-03-27 16:59:03 +0100

    Stage 1 complete

--- scripts/newFlowChart.js
+++ scripts/newFlowChart.js
@@ -1,8 +1,10 @@
 // Variables
+let canvasHeight = null;
 let blockWidth = null;
 let blockHeight = null;
 let hSpace = null;
 let vSpace = null;
+let vPadding = null;
 let activeBlock = null;
 let blocks = {};
 let cards = {};
@@ -10,6 +12,7 @@ let connectors = {};
 
 // Constants
 const fontSize = 18;
+const textPadding = 10;
 const strokeWidth = 3;
 const strokeColors = {
     "S" : "#c86419ff", // System
@@ -29,7 +32,7 @@ const container = document.getElementsByClassName('stage1-container')[0];
 const canvas = new fabric.Canvas('stage1Flowchart', { 
     selection: false, // Disable box selection
     allowTouchScrolling: true // Enable touch scrolling
-}); 
+});
 
 // Create a task block
 function createBlock(blockData) {
@@ -41,7 +44,7 @@ function createBlock(blockData) {
         width: blockWidth,
         height: blockHeight,
         stroke: strokeColors[blockData.id[0]],
-        strokeWidth: 3,
+        strokeWidth: strokeWidth,
         selectable: false
     });
 
@@ -62,7 +65,7 @@ function createBlock(blockData) {
     // Group the block and its text
     const group = new fabric.Group([block, text], {
         left: blockData.col*blockWidth + (blockData.col+1)*hSpace,
-        top: blockData.row*blockHeight + blockData.row*vSpace + hSpace,
+        top: blockData.row*blockHeight + blockData.row*vSpace + vPadding,
         selectable: false,
         hoverCursor: 'default'
     });
@@ -94,13 +97,13 @@ function createCard(blockData) {
     // Card dimensions
     const cardWidth = 3 * blockWidth + 2 * hSpace;
     const cardLeft = hSpace;
-    const cardTop = blockData.row*blockHeight + blockData.row*vSpace + hSpace; // Initial top position
+    const cardTop = blockData.row*blockHeight + blockData.row*vSpace + vPadding; // Initial top position
 
     // Create the title text
     const title = new fabric.Textbox(blockData.title, {
-        left: cardLeft + 10,
+        left: cardLeft + textPadding,
         top: cardTop,
-        width: cardWidth - 40, // Add some padding for the title width
+        width: cardWidth - 2*textPadding,
         fontSize: fontSize,
         fontFamily: 'Verdana',
         fill: 'white',
@@ -110,7 +113,7 @@ function createCard(blockData) {
     });
 
     // Dynamically calculate the header height based on the title height
-    const headerHeight = title.height + 10; // Add padding for the header
+    const headerHeight = title.height + textPadding; // Add padding for the header
 
     // Create the card background
     const card = new fabric.Rect({
@@ -120,7 +123,7 @@ function createCard(blockData) {
         height: 0, // Temporary height, will be updated later
         fill: 'white',
         stroke: strokeColors[blockData.id[0]],
-        strokeWidth: 3,
+        strokeWidth: strokeWidth,
         selectable: false,
         hoverCursor: 'default'
     });
@@ -140,14 +143,15 @@ function createCard(blockData) {
     title.set('top', cardTop + headerHeight / 2 - title.height / 2);
 
     // Add the "close card" icon
-    const closeIcon = new fabric.Text('✖', {
-        left: cardLeft + cardWidth - 30,
+    const closeIcon = new fabric.Text('✕', {
+        left: cardLeft + cardWidth - 3*textPadding,
         top: cardTop + headerHeight / 2 - fontSize / 2,
         fontSize: fontSize,
         fontFamily: 'Verdana',
         fill: 'white',
         selectable: false,
-        hoverCursor: 'pointer'
+        hoverCursor: 'pointer',
+        fontWeight: 'bold'
     });
 
     // Add event listener for the closeIcon
@@ -157,11 +161,12 @@ function createCard(blockData) {
     });
 
     // Add the main content (description and links)
+    const textSize = fontSize - 2;
     const content = new fabric.Textbox(blockData.description + '\n\nLinks:', {
-        left: cardLeft + 10,
-        top: cardTop + headerHeight + 10,
-        width: cardWidth - 20,
-        fontSize: fontSize - 2,
+        left: cardLeft + textPadding,
+        top: cardTop + headerHeight + textPadding,
+        width: cardWidth - 2*textPadding,
+        fontSize: textSize,
         fontFamily: 'Verdana',
         fill: '#000000',
         textAlign: 'left',
@@ -170,32 +175,35 @@ function createCard(blockData) {
     });
 
     // Add the links as clickable text
-    const linkStartTop = content.top + content.height + 10; // Start position for links
+    let linkStartTop = content.top + content.height + textPadding; // Start position for links
     const linkElements = blockData.links.map((link, index) => {
         // Create the bullet as a separate text object
         const bullet = new fabric.Text('•', {
-            left: cardLeft + 10,
-            top: linkStartTop + index * (fontSize + 5), // Space between links
-            fontSize: fontSize - 2,
+            left: cardLeft + textPadding,
+            top: linkStartTop,
+            fontSize: textSize,
             fontFamily: 'Verdana',
-            fill: '#000000', // Black color for the bullet
+            fill: 'black',
             textAlign: 'left',
             selectable: false,
             hoverCursor: 'default'
         });
 
         // Create the link text with underline
-        const linkText = new fabric.Text(link[0], {
-            left: cardLeft + 25, // Adjust position to leave space for the bullet
-            top: linkStartTop + index * (fontSize + 5), // Space between links
-            fontSize: fontSize - 2,
+        const linkText = new fabric.Textbox(link[0], {
+            left: cardLeft + 2.5*textPadding,
+            top: linkStartTop,
+            width: cardWidth - 3.5*textPadding,
+            fontSize: textSize,
             fontFamily: 'Verdana',
-            fill: '#0000EE', // Blue color for links
+            fill: 'blue',
             textAlign: 'left',
             selectable: false,
             hoverCursor: 'pointer',
-            underline: true // Underline only the link text
+            underline: true
         });
+
+        linkStartTop += linkText.height + 0.5*textPadding;
 
         // Add click event to open the link
         linkText.on('mousedown', () => {
@@ -206,14 +214,18 @@ function createCard(blockData) {
     });
 
     // Adjust the card height based on the content and links
-    const cardHeight = headerHeight + content.height + 20 + linkElements.length * (fontSize + 5);
+    const linksHeight = linkStartTop - content.top - content.height - textPadding;
+    const cardHeight = headerHeight + content.height + 2*textPadding + linksHeight;
     card.set('height', cardHeight);
 
     // Update the positions of header and content
     header.set('top', cardTop);
-    content.set('top', cardTop + headerHeight + 10);
+    content.set('top', cardTop + headerHeight + textPadding);
 
-    return [card, header, title, closeIcon, content, ...linkElements.flat()];
+    // Shift the elements it needed
+    elements = [card, header, title, closeIcon, content, ...linkElements.flat()];
+
+    return elements;
 }
 
 // Update the cards
@@ -229,29 +241,83 @@ async function updateCards() {
     .catch(error => console.error('Error loading flowchart data:', error));
 }
 
-function createConnector(fromBlock, toBlock, type="straight") {
-    const from = {
-        x: fromBlock.left + blockWidth / 2,
-        y: fromBlock.top + blockHeight
-    };
-    const to = {
-        x: toBlock.left + blockWidth / 2,
-        y: toBlock.top
+function createConnector(connectorData) {
+
+    let fromBlock = connectorData.from ? blocks[connectorData.from] : undefined;
+    let toBlock = connectorData.to ? blocks[connectorData.to] : undefined;
+    let fromOffset = connectorData.fromOffset ?? 0;
+    let toOffset = connectorData.toOffset ?? 0;
+    let type = connectorData.type ?? "straight";
+    let fromSide = connectorData.fromSide ?? "bottom";
+    let toSide = connectorData.toSide ?? "top";
+
+    let from = null;
+    if(!fromBlock){
+        from = {
+            x: toBlock.left + blockWidth / 2 + fromOffset*hSpace,
+            y: 0
+        };        
+    }else{
+        if (fromSide === "bottom") {
+            from = {
+                x: fromBlock.left + blockWidth / 2 + fromOffset*hSpace,
+                y: fromBlock.top + blockHeight
+            };
+        } else if (fromSide === "right") {
+            from = {
+                x: fromBlock.left + blockWidth,
+                y: fromBlock.top + blockHeight / 2 + fromOffset*hSpace
+            };
+        } else {
+            console.error(`Unknown side: ${fromSide}`);
+            return null;
+        }
+    }
+
+    let to = null;
+    if(!toBlock){
+        to = {
+            x: fromBlock.left + blockWidth / 2 + fromOffset*hSpace,
+            y: canvas.height
+        };        
+    }else{
+        if (toSide === "top") {
+            to = {
+                x: toBlock.left + blockWidth / 2 + toOffset*hSpace,
+                y: toBlock.top
+            };
+        } else if (toSide === "left") {
+            to = {
+                x: toBlock.left,
+                y: toBlock.top + blockHeight / 2 + toOffset*hSpace
+            };
+        } else {
+            console.error(`Unknown side: ${toSide}`);
+            return null;
+        }
+    }
+
+    const mid = {
+        x: (from.x + to.x) / 2,
+        y: (from.y + to.y) / 2
     };
 
     // Define the path for the arrow
     let path;
+    let angle;
     if (type === "straight") {
-        // Straight line arrow
         path = `M ${from.x} ${from.y} L ${to.x} ${to.y}`;
+        angle = Math.atan2(to.y - from.y, to.x - from.x);
+    } else if (type === "zigzag") {
+        path = `M ${from.x} ${from.y} L ${from.x} ${mid.y} L ${to.x} ${mid.y} L ${to.x} ${to.y}`;
+        angle = Math.atan2(to.y - mid.y, to.x - to.x);
     } else {
         console.error(`Unknown arrow type: ${type}`);
         return null;
     }
 
     // Add an arrowhead to the path
-    const arrowHeadSize = 10;
-    const angle = Math.atan2(to.y - from.y, to.x - from.x);
+    const arrowHeadSize = hSpace/2;
     const arrowHead = [
         {
             x: to.x - arrowHeadSize * Math.cos(angle - Math.PI / 6),
@@ -270,8 +336,8 @@ function createConnector(fromBlock, toBlock, type="straight") {
 
     // Create the fabric.Path object
     const arrow = new fabric.Path(path, {
-        fill: strokeColors[fromBlock.id[0]],
-        stroke: strokeColors[fromBlock.id[0]],
+        fill: "transparent",
+        stroke: fromBlock ? strokeColors[fromBlock.id[0]] : strokeColors["C"],
         strokeWidth: strokeWidth,
         selectable: false,
         hoverCursor: "default"
@@ -290,7 +356,7 @@ async function updateGrid() {
             blocks[blockData.id] = createBlock(blockData);
         });
         data['connectors'].forEach(connectorData => {
-            connectors[connectorData.from + "-" + connectorData.to] = createConnector(blocks[connectorData.from], blocks[connectorData.to]);
+            connectors[connectorData.from + "-" + connectorData.to] = createConnector(connectorData);
         });
     })
     .catch(error => console.error('Error loading flowchart data:', error));
@@ -298,26 +364,40 @@ async function updateGrid() {
 
 // Draw the canvas
 function draw() {
+    // Clear the canvas and reset the height
     canvas.clear();
+    canvas.setHeight(canvasHeight);
+
+    // Add all elements
     canvas.add(...Object.values(connectors));
     canvas.add(...Object.values(blocks));
     if (activeBlock) {
         canvas.add(...cards[activeBlock]);
+        const overflow = cards[activeBlock][0].top + cards[activeBlock][0].height + strokeWidth - canvas.height;
+        if (overflow > 0) {
+            canvas.setHeight(canvas.height + overflow + vPadding);
+        }
     }
+
+    // Render all elements
     canvas.renderAll();    
 }
 
 // Function to set canvas dimensions to match the container
 async function updateCanvas() {
+
     canvas.setWidth(container.clientWidth);
-    canvas.setHeight(container.clientHeight);
 
     // Set the grid dimensions
     blockWidth  = 0.3*canvas.width;
-    blockHeight = 0.5*blockWidth;
+    blockHeight = 0.6*blockWidth;
     hSpace = 0.025*canvas.width;
-    vSpace = 4*hSpace
-    
+    vSpace = 4*hSpace;
+    vPadding = 2*hSpace;
+    canvasHeight = 2*blockHeight + vSpace + 2*vPadding;
+
+    canvas.setHeight(canvasHeight);
+
     // Update the grid
     await updateGrid();
     await updateCards();
